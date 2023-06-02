1c265ca6621a372bfc6b839b416cab80d9f168de
Jordan Cardenas <jordancardenas@Jordans-MBP.attlocal.net>
2023-06-02 11:03:55 -0700

    server.js error fixed

--- server/server.js
+++ server/server.js
@@ -1,48 +1,37 @@
 const express = require("express");
-const path = require("path");
 const { ApolloServer } = require("apollo-server-express");
+const path = require("path");
+const db = require("./config/connection");
 const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require("./utils/auth");
 
-async function startApolloServer() {
-  const app = express();
-  const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
+const app = express();
 
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: authMiddleware,
-  });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers, 
+  context: authMiddleware,
+  persistedQueries: false, 
+});
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+};
+
+const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
-
   server.applyMiddleware({ app });
 
-  app.use(express.urlencoded({ extended: true }));
-  app.use(express.json());
-
-  // Serve static assets if in production
-  if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../client/build")));
-    app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`Server now running on port ${PORT}!`);
     });
-  }
-
-  // Connect to MongoDB and start the server
-  await mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/booksearch", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
   });
+};
 
-  app.listen(PORT, () => {
-    console.log(`🌍 Server listening on http://localhost:${PORT}`);
-    console.log(`🚀 GraphQL server ready at http://localhost:${PORT}${server.graphqlPath}`);
-  });
-}
-
-startApolloServer().catch((error) => {
-  console.log("Error starting Apollo Server:", error);
-});
+startApolloServer(typeDefs, resolvers);
